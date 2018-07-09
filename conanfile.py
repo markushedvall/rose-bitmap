ecfd5e8237faa8816f86533866cc6dece1e27cc3
Markus Hedvall <mackanhedvall@gmail.com>
2018-07-09 11:46:25 +0800

    Add unit tests

--- conanfile.py
+++ conanfile.py
@@ -7,8 +7,17 @@ class RoseBitmapConan(ConanFile):
     license = "MIT OR Apache-2.0"
     url = "https://github.com/markushedvall/rose-bitmap"
     description = "Single header library for a simple bitmap structure"
-    exports_sources = "include/*"
+    build_requires = "gtest/1.8.0@bincrafters/stable"
+    settings = "os", "compiler", "arch", "build_type"
+    generators = "cmake"
+    exports_sources = "include/*", "test/*", "CMakeLists.txt"
     no_copy_source = True
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.test()
 
     def package(self):
         self.copy("*.hpp")
---
+++ test/bitmap_test.cpp
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+#include <rose/bitmap.hpp>
+
+using rose::Bitmap;
+using rose::bitmap::Fmt;
+
+TEST(Bitmap, InitializerList) {
+  Bitmap<Fmt::RGB> bitmap = {1024, 512, nullptr};
+}
+
+TEST(Bitmap, width) {
+  Bitmap<Fmt::RGB> bitmap = {1024, 512, nullptr};
+  EXPECT_TRUE(bitmap.width() == 1024);
+}
+
+TEST(Bitmap, height) {
+  Bitmap<Fmt::RGB> bitmap = {1024, 512, nullptr};
+  EXPECT_TRUE(bitmap.height() == 512);
+}
+
+TEST(Bitmap, data) {
+  uint8_t pixel[] = {0x0, 0x80, 0xFF};
+  Bitmap<Fmt::RGB> bitmap = {1024, 512, pixel};
+  EXPECT_TRUE(bitmap.data() == pixel);
+}
+
+TEST(Bitmap, format) {
+  Bitmap<Fmt::RGB> rgb_bitmap = {1024, 512, nullptr};
+  EXPECT_TRUE(rgb_bitmap.format() == Fmt::RGB);
+  Bitmap<Fmt::RGBA> rgba_bitmap = {1024, 512, nullptr};
+  EXPECT_TRUE(rgba_bitmap.format() == Fmt::RGBA);
+}
+
+TEST(Bitmap, channels) {
+  EXPECT_TRUE(rose::bitmap::channels<Fmt::RGB>() == 3);
+  EXPECT_TRUE(rose::bitmap::channels<Fmt::RGBA>() == 4);
+}
