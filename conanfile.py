2e4c723087c77de81d0efc84964ebae485a8359c
Markus Hedvall <mackanhedvall@gmail.com>
2018-07-09 14:28:41 +0800

    v0.2.0

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class RoseBitmapConan(ConanFile):
     name = "rose-bitmap"
-    version = "0.1.0"
+    version = "0.2.0"
     license = "MIT OR Apache-2.0"
     url = "https://github.com/markushedvall/rose-bitmap"
     description = "Single header library for a simple bitmap structure"
